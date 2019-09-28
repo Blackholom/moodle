3ee1a79a3ec0c05851cff5ec5531ddbfd71d76c5
Blackholom <kevinmega94@hotmail.com>
2019-09-27 20:52:50 -0500

    Se añaden funciones de backend para el CRUD de usuarios, asignaturas y tipo de usuario

--- server/controller.js
+++ server/controller.js
@@ -5,15 +5,51 @@ module.exports = http.createServer((req, res) => {
     const service = require('./service');
     const reqUrl = url.parse(req.url, true);
 
-    if (reqUrl.pathname == '/students' && req.method === 'GET'){
+    if (reqUrl.pathname == '/user' && req.method === 'GET'){
         console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
-        service.getAllStudents(req, res);
-    } else if (reqUrl.pathname == '/students/new' && req.method === 'GET'){
+        service.getAllUsers(req, res);
+    } else if (reqUrl.pathname == '/user/new' && req.method === 'GET'){
         console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
-        service.createStudent(req, res);
-    } else if (reqUrl.pathname == '/students/show' && req.method === 'GET'){
+        service.createUser(req, res);
+    } else if (reqUrl.pathname == '/user/edit' && req.method === 'GET'){
         console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
-        service.getStudentById(req, res);
+        service.updateUser(req, res);
+    } else if (reqUrl.pathname == '/user/delete' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.deleteUser(req, res);
+    } else if (reqUrl.pathname == '/user/show' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.getUserById(req, res);
+    } else if (reqUrl.pathname == '/subject' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.getAllSubject(req, res);
+    } else if (reqUrl.pathname == '/subject/new' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.createSubject(req, res);
+    } else if (reqUrl.pathname == '/subject/edit' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.updateSubject(req, res);
+    } else if (reqUrl.pathname == '/subject/delete' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.deleteSubject(req, res);
+    } else if (reqUrl.pathname == '/subject/show' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.getSubjectById(req, res);
+    } else if (reqUrl.pathname == '/userType' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.getAllUserTypes(req, res);
+    } else if (reqUrl.pathname == '/userType/new' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.createUserType(req, res);
+    } else if (reqUrl.pathname == '/userType/edit' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.updateUseType(req, res);
+    } else if (reqUrl.pathname == '/userType/delete' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.deleteUserType(req, res);
+    } else if (reqUrl.pathname == '/userType/show' && req.method === 'GET'){
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+        service.getUserTypeById(req, res);
     } else if (reqUrl.pathname == '/test' && req.method === 'POST') {
         // TODO
     } else {
--- server/service.js
+++ server/service.js
@@ -8,18 +8,10 @@ const conn = mysql.createConnection({
     database: 'moodle_db_dev'
 });
 
+// CRUD User
+// Get list of all students
 exports.getAllUsers = function (req, res) {
-    conn.query('SELECT * FROM user', (error, rows) => {
-        if (error) throw error;
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({"status": 200, "error": null, "response": rows}))
-    });
-};
-
-exports.getAllStudents = function (req, res) {
-    conn.query('SELECT u.dni,u.name FROM user u WHERE u.type_id = 2', (error, rows) => {
+    conn.query('SELECT u.id,u.dni,u.name,u.state FROM user u', (error, rows) => {
         if (error) throw error;
 
         res.statusCode = 200;
@@ -28,27 +20,82 @@ exports.getAllStudents = function (req, res) {
     });
 };
 
-exports.createStudent = function (req, res) {
+// Create User
+exports.createUser = function (req, res) {
     const reqUrl = url.parse(req.url, true);
-    const student = { name: reqUrl.query.name, type_id: 2, dni: reqUrl.query.dni, created_at: new Date() };
+    const student = { name: reqUrl.query.name, type_id: reqUrl.query.type_id, dni: reqUrl.query.dni, pin: reqUrl.query.pin, created_at: new Date() };
 
-    conn.query('INSERT INTO user SET ?', student, (error, rows) => {
-        if (error) throw error;
+    conn.query('INSERT INTO user SET ?', student, (e, rows) => {
+        var result = rows;
+        var error = null;
 
-        res.statusCode = 200;
+        if (e){
+            error = 'Ocurrio un error durante la creación del registro. ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro se creó exitosamente!';
+            res.statusCode = 200;
+        }
+
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({"status": 200, "error": null, "response": rows}))
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
     });
 };
 
-exports.getStudentById = function (req, res) {
+// Update User
+exports.updateUser = function (req, res) {
     const reqUrl = url.parse(req.url, true);
-    const student_id = reqUrl.query.id;
-    conn.query('SELECT u.dni,u.name FROM user u WHERE u.type_id = 2 AND u.id = ?', student_id, (error, rows) => {
-        if (error) throw error;
+    const name = reqUrl.query.name, type_id = reqUrl.query.type_id, dni = reqUrl.query.dni, pin = reqUrl.query.pin, update = new Date(), state = reqUrl.query.state, id = reqUrl.query.id;
+    const updated_at = update.getFullYear() + "-" + (update.getMonth() + 1) + "-" + update.getDate() + " " + update.getHours() + ":" + update.getMinutes() + ":" + update.getSeconds()
 
-        if (rows.length == 0){
-            error = 'No hay registros con el id indicado'
+    conn.query('UPDATE user u SET u.name = "' + name + '",u.type_id = ' + type_id + ',u.dni = "' + dni + '",u.pin = "' + pin + '",u.updated_at = "' + updated_at.toLocaleString() + '",u.state = ' + state + ' WHERE u.id = ' + id, (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la actualización del registro. ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro se ha actualizado con éxito';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Delete User
+exports.deleteUser = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const id = reqUrl.query.id;
+
+    conn.query('DELETE FROM user WHERE id = ?', parseInt(id), (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la eliminación del registro. ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro ha sido eliminado con éxito';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Get user by id
+exports.getUserById = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const id = reqUrl.query.id;
+    conn.query('SELECT u.dni,u.name,u.state FROM user u WHERE u.id = ?', id, (e, rows) => {
+        var error = null;
+
+        if (e){
+            error = 'No hay registros con el id indicado. ' + e;
             res.statusCode = 404;
         } else {
             res.statusCode = 200;
@@ -59,26 +106,257 @@ exports.getStudentById = function (req, res) {
     });
 };
 
-exports.testRequest = function (req, res) {
-    body = '';
-
-    req.on('data', function (chunk) {
-        body += chunk;
-    });
-
-    req.on('end', function () {
-
-        postBody = JSON.parse(body);
-
-        var response = {
-            "text": "Post Request Value is  " + postBody.value
-        };
+// ------------------------------------------------------------------------------------------------------
+// CRUD Subject
+// Get list of all subjects
+exports.getAllSubject = function (req, res) {
+    conn.query('SELECT s.id,s.name,s.description,s.state FROM subject s', (error, rows) => {
+        if (error) throw error;
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
+        res.end(JSON.stringify({"status": 200, "error": error, "response": rows}))
     });
 };
+
+// Create Subject
+exports.createSubject = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const subject = { name: reqUrl.query.name, description: reqUrl.query.description, quota: reqUrl.query.quota, created_at: new Date() };
+
+    conn.query('INSERT INTO subject SET ?', subject, (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la creación del registro. Error: ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro se creó exitosamente!';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Update Subject
+exports.updateSubject = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const name = reqUrl.query.name, description = reqUrl.query.description, quota = reqUrl.query.quota, state = reqUrl.query.state, update = new Date(), id = reqUrl.query.id;
+    const updated_at = update.getFullYear() + "-" + (update.getMonth() + 1) + "-" + update.getDate() + " " + update.getHours() + ":" + update.getMinutes() + ":" + update.getSeconds()
+
+    conn.query('UPDATE subject SET s.name = "' + name + '",s.description = "' + description + '",s.quota = ' + quota + ',s.state = ' + state + ',s.updated_at = "' + updated_at.toLocaleString() + '" WHERE s.id = ' + id, (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la actualización del registro. Error: ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro se ha actualizado con éxito';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Delete Subject
+exports.deleteSubject = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const id = reqUrl.query.id;
+
+    conn.query('DELETE FROM subject WHERE id = ?', parseInt(id), (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la eliminación del registro. Error: ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro ha sido eliminado con éxito';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Get subject by id
+exports.getSubjectById = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const id = reqUrl.query.id;
+    conn.query('SELECT s.id,s.name,s.description,s.state FROM subject s WHERE s.id = ?', id, (e, rows) => {
+        var error = null;
+
+        if (e){
+            error = 'No hay registros con el id indicado. Error: ' + e;
+            res.statusCode = 404;
+        } else {
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": res.statusCode, "error": error, "response": rows}))
+    });
+};
+
+// ------------------------------------------------------------------------------------------------------
+// CRUD User Type
+// Get list of all User Type
+exports.getAllUserTypes = function (req, res) {
+    conn.query('SELECT t.id,t.name,t.description,t.state FROM user_type t', (error, rows) => {
+        if (error) throw error;
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": rows}))
+    });
+};
+
+// Create User Type
+exports.createUserType = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const subject = { name: reqUrl.query.name, description: reqUrl.query.description, created_at: new Date() };
+
+    conn.query('INSERT INTO user_type SET ?', subject, (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la creación del registro.' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro se creó exitosamente!';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Update User Type
+exports.updateUseType = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const name = reqUrl.query.name, description = reqUrl.query.description, state = reqUrl.query.state, update = new Date(), id = reqUrl.query.id;
+    const updated_at = update.getFullYear() + "-" + (update.getMonth() + 1) + "-" + update.getDate() + " " + update.getHours() + ":" + update.getMinutes() + ":" + update.getSeconds()
+
+    conn.query('UPDATE user_type t SET t.name = "' + name + '",t.description = "' + description + '",t.state = ' + state + ',t.updated_at = "' + updated_at.toLocaleString() + '" WHERE t.id = ' + id, (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la actualización del registro. ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro se ha actualizado con éxito';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Delete User Type
+exports.deleteUserType = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const id = reqUrl.query.id;
+
+    conn.query('DELETE FROM user_type WHERE id = ?', id, (e, rows) => {
+        var result = rows;
+        var error = null;
+
+        if (e){
+            error = 'Ocurrio un error durante la eliminación del registro. ' + e;
+            res.statusCode = 404;
+        } else {
+            result = 'El registro ha sido eliminado con éxito';
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": 200, "error": error, "response": result}))
+    });
+};
+
+// Get User Type by id
+exports.getUserTypeById = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+    const id = reqUrl.query.id;
+    conn.query('SELECT t.id,t.name,t.description,t.state FROM user_type t WHERE t.id = ?', id, (e, rows) => {
+        var error = null;
+
+        if (e){
+            error = 'No hay registros con el id indicado. ' + e;
+            res.statusCode = 404;
+        } else {
+            res.statusCode = 200;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status": res.statusCode, "error": error, "response": rows}))
+    });
+};
+
+
+
+
+
+// exports.getStudentById = function (req, res) {
+//     const reqUrl = url.parse(req.url, true);
+//     const student_id = reqUrl.query.id;
+//     conn.query('SELECT u.dni,u.name FROM user u WHERE u.type_id = 2 AND u.id = ?', student_id, (error, rows) => {
+//         if (error) throw error;
+//
+//         if (rows.length == 0){
+//             error = 'No hay registros con el id indicado'
+//             res.statusCode = 404;
+//         } else {
+//             res.statusCode = 200;
+//         }
+//
+//         res.setHeader('Content-Type', 'application/json');
+//         res.end(JSON.stringify({"status": res.statusCode, "error": error, "response": rows}))
+//     });
+// };
+//
+// // Get list of all students
+// exports.getAllStudents = function (req, res) {
+//     conn.query('SELECT u.dni,u.name FROM user u WHERE u.type_id = 2', (error, rows) => {
+//         if (error) throw error;
+//
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'application/json');
+//         res.end(JSON.stringify({"status": 200, "error": error, "response": rows}))
+//     });
+// };
+//
+// exports.testRequest = function (req, res) {
+//     body = '';
+//
+//     req.on('data', function (chunk) {
+//         body += chunk;
+//     });
+//
+//     req.on('end', function () {
+//
+//         postBody = JSON.parse(body);
+//
+//         var response = {
+//             "text": "Post Request Value is  " + postBody.value
+//         };
+//
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'application/json');
+//         res.end(JSON.stringify(response));
+//     });
+// };
 
 exports.invalidRequest = function (req, res) {
     res.statusCode = 404;
